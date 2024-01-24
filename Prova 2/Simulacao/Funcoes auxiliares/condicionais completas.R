377e28970aeb861fdc879e5ff8410d89416739e5
Bola382e <marcus.gabriel.silva@hotmail.com>
2024-01-23 21:37:00 -0400

    inicio da implementação do TS

--- Prova 2/Simulacao/Funcoes auxiliares/condicionais completas.R
+++ Prova 2/Simulacao/Funcoes auxiliares/condicionais completas.R
@@ -149,3 +149,133 @@ full_Z = function(prob,beta,tau2,Delta,nu){
  }
  return(out)
 }
+
+# ==============================================================================
+#                                  Telescoping
+# ==============================================================================
+full_Z.TS = function(G,prob,beta,tau2,Delta,nu){
+ aux_mu = X%*%beta+matrix(rep(b*Delta,n), nrow = n, ncol = G, byrow = T)
+ aux_sig = sqrt(tau2+Delta^2)
+ aux_lam = Delta/sqrt(tau2)
+ out = NULL
+ for(k in 1:n){
+  aux_prob = exp(log(prob)+sapply(1:G,function(a) dst(y[k], aux_mu[k,a], aux_sig[a], aux_lam[a], nu,log=T))) # (eta1,eta2,eta3) x (st1,st2,st3)
+  out[k] = sample(1:G,1, prob = aux_prob) # prob eh normalizado internamente
+ }
+ return(out)
+}
+
+full_beta.TS = function(j,tau2,Delta){
+ # Xj, Uj, yj, tj, M ja devem estar especificados
+ if(M[j]==1){ # o R trata uma unica linha de uma matriz como um vetor coluna
+  XU = as.matrix(Xj)%*%Uj # t(Xj)%*%Uj
+ }else{
+  XU = crossprod(Xj,Uj) # t(Xj)%*%Uj
+ } 
+ aux_y = yj-(b+tj)*Delta[j] 
+ sigma_inv = XU%*%Xj + diag(tau2[j]/c^2,nrow = p)
+ 
+ # ----------------------
+ # Atualizando beta
+ # ----------------------
+ s_sigma = chol2inv(chol(sigma_inv)) 
+ mmm = solve(sigma_inv,XU%*%aux_y)  
+ ppp = tau2[j]*s_sigma      
+ # novo valor pro vetor beta_j
+ MASS::mvrnorm(1, mu = mmm,Sigma = ppp)
+}
+
+full_tau2.TS = function(j,beta,Delta){
+ # Xj, Uj, yj, tj, M, r, s ja devem estar especificados
+ S3 = sum(uj*(yj-Xj%*%beta[,j]-(b+tj)*Delta[j])^2)
+ 
+ 1/rgamma(1, shape = M[j]/2+r, rate = (S3+s)/2)
+}
+
+full_Delta.TS = function(j,beta,tau2){
+ # Xj, Uj, yj, tj, M, omega ja devem estar especificados
+ S1 = sum(uj*(yj-Xj%*%beta[,j])*(b+tj))
+ S2 = sum(uj*(b+tj)^2)
+ denom = omega^2*S2 + tau2[j]
+ 
+ # aqui tomamos eta = 0 para evitar mais conta, lembrar de 
+ # alterar a media caso eta != 0.
+ rnorm(1, omega^2*S1/denom, omega*sqrt(tau2[j]/denom))
+}
+
+full_U.TS = function(tau2,Delta,nu,t,z){
+ # aux_mu, aux_yxbeta ja devem estar declarados
+ out = NULL
+ for(k in 1:n){
+  # atualizando U
+  D1 =(aux_yxbeta[k,z[k]]-t[k]*Delta[z[k]])^2/(2*tau2[z[k]]) 
+  
+  out[k] = rgamma(1, shape = nu/2+1, rate = D1 + (t[k]^2+nu)/2)
+ }
+ return(out)
+}
+
+full_T.TS = function(tau2,Delta,u,z){
+ # aux_mu, aux_yxbeta ja devem estar declarados
+ out = NULL
+ for(k in 1:n){
+  aux_denom = Delta[z[k]]^2 + tau2[z[k]]
+  aux_mean = (aux_yxbeta[k,z[k]]*Delta[z[k]])/aux_denom
+  aux_sd = sqrt(tau2[z[k]]/(u[k]*aux_denom))
+  
+  aux_truncnorm = rtruncnorm(1,-aux_mean/aux_sd)$amostra
+  
+  out[k] = aux_mean+aux_sd*aux_truncnorm
+ }
+ return(out)
+}
+
+full_K.TS = function(Gplus,Gmax,M,gammaProb, lpriori){
+ options(digits=10)
+ lpriori = ifelse(lpriori == "unif",0,Brobdingnag::as.brob(lpriori)) # vetor com lprioris de K
+ 
+ # para G = Gplus
+ gammaRatio = gammaProb/Gplus
+ 
+ # para calcular o somatorio
+ aux = sapply(1:Gplus, function(a) lgamma(M[a] + gammaRatio) - lgamma(1 + gammaRatio))
+ 
+ # numeros estupidamente grandes nao importa oq eu faca
+ # pacote Brobdingnag para tratar destes numeros
+ aux = Brobdingnag::as.brob(aux)
+ 
+ lprob_K = lpriori + 
+  Brobdingnag::as.brob(Gplus*log(gammaProb))- 
+  Brobdingnag::as.brob(Gplus*log(Gplus))+ 
+  Brobdingnag::as.brob(lfactorial(Gplus)) - 
+  Brobdingnag::as.brob(lfactorial(Gplus-Gplus)) + 
+  sum(aux)
+ 
+ # para os demais G
+ 
+ for(G in (Gplus+1):Gmax){
+  gammaRatio = gammaProb/G
+  
+  # para calcular o somatorio
+  aux = sapply(1:Gplus, function(a) lgamma(M[a] + gammaRatio) - lgamma(1 + gammaRatio))
+  
+  aux = Brobdingnag::as.brob(aux)
+  
+  lprob_K = Brobdingnag::cbrob(lprob_K,lpriori + 
+                       Brobdingnag::as.brob(Gplus*log(gammaProb))- 
+                       Brobdingnag::as.brob(Gplus*log(G))+ 
+                       Brobdingnag::as.brob(lfactorial(G)) - 
+                       Brobdingnag::as.brob(lfactorial(G-Gplus)) + 
+                       sum(aux))
+ }
+ # normalizando prob
+ prob_K = exp(lprob_K)
+ norm_K = sum(prob_K)
+ 
+ prob_K = as.numeric(prob_K/norm_K)
+ 
+ aux2 = c(rmultinom(1,1,prob_K))
+ 
+ Gplus + which(aux2==1)-1
+ options(digits=7)
+}
